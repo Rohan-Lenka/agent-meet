fcf8f9e56f029ea30de26547031aa9a081d71160
Rohan Lenka <rohanlenka999@gmail.com>
2026-01-05 13:47:47 +0530

    added protectedProcedure

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,13 +1,31 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import {
+  baseProcedure,
+  createTRPCRouter,
+  protectedProcedure,
+} from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
+import { agentsInsertSchema } from "../schemas";
 
 export const AgentsRouter = createTRPCRouter({
+  // change to protectedProcedure later
   getMany: baseProcedure.query(async () => {
     const data = await db.select().from(agents);
-    // for testing error-state 
+    // for testing error-state
     // throw new TRPCError({ message: "error testing", code: "BAD_GATEWAY" })
     return data;
   }),
+  create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+      return createdAgent;
+    }),
 });
