cda4d1d573d2547a2e9a41c08c6cfa6763a3f85f
Rohan Lenka <rohanlenka999@gmail.com>
2026-01-12 00:44:06 +0530

    few refactors and meetings table

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -9,8 +9,9 @@ import {
   MAX_PAGE_SIZE,
   MIN_PAGE_SIZE,
 } from "@/constants";
-import { meetings } from "@/db/schema";
+import { agents, meetings } from "@/db/schema";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
+import { MeetingStatus } from "../types";
 
 export const MeetingsRouter = createTRPCRouter({
   create: protectedProcedure
@@ -70,51 +71,71 @@ export const MeetingsRouter = createTRPCRouter({
       return existingMeeting;
     }),
 
-  getMany: protectedProcedure
+    getMany: protectedProcedure
     .input(
-      z.object({
-        page: z.number().default(DEFAULT_PAGE),
-        pageSize: z
-          .number()
-          .min(MIN_PAGE_SIZE)
-          .max(MAX_PAGE_SIZE)
-          .default(DEFAULT_PAGE_SIZE),
-        search: z.string().nullish(),
-      })
+        z.object({
+            page: z.number().default(DEFAULT_PAGE),
+            pageSize: z
+                .number()
+                .min(MIN_PAGE_SIZE)
+                .max(MAX_PAGE_SIZE)
+                .default(DEFAULT_PAGE_SIZE),
+            search: z.string().nullish(),
+            agentId: z.string().nullish(),
+            status: z
+                .enum([
+                    MeetingStatus.Upcoming,
+                    MeetingStatus.Active,
+                    MeetingStatus.Completed,
+                    MeetingStatus.Processing,
+                    MeetingStatus.Cancelled,
+                ])
+                .nullish(),
+        })
     )
     .query(async ({ ctx, input }) => {
-      const { search, page, pageSize } = input;
-      const data = await db
-        .select({
-          ...getTableColumns(meetings),
-        })
-        .from(meetings)
-        .where(
-          and(
-            eq(meetings.userId, ctx.auth.user.id),
-            search ? ilike(meetings.name, `%${search}%`) : undefined
-          )
-        )
-        .orderBy(desc(meetings.createdAt), desc(meetings.id))
-        .limit(pageSize)
-        .offset((page - 1) * pageSize);
+        const { search, page, pageSize, status, agentId } = input;
+        const data = await db
+            .select({
+                ...getTableColumns(meetings),
+                agent: agents,
+                duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as(
+                    "duration"
+                ),
+            })
+            .from(meetings)
+            .innerJoin(agents, eq(meetings.agentId, agents.id))
+            .where(
+                and(
+                    eq(meetings.userId, ctx.auth.user.id),
+                    search ? ilike(meetings.name, `%${search}%`) : undefined,
+                    status ? eq(meetings.status, status) : undefined,
+                    agentId ? eq(meetings.agentId, agentId) : undefined
+                )
+            )
+            .orderBy(desc(meetings.createdAt), desc(meetings.id))
+            .limit(pageSize)
+            .offset((page - 1) * pageSize);
 
-      const [total] = await db
-        .select({ count: count() })
-        .from(meetings)
-        .where(
-          and(
-            eq(meetings.userId, ctx.auth.user.id),
-            search ? ilike(meetings.name, `%${search}%`) : undefined
-          )
-        );
+        const [total] = await db
+            .select({ count: count() })
+            .from(meetings)
+            .innerJoin(agents, eq(meetings.agentId, agents.id))
+            .where(
+                and(
+                    eq(meetings.userId, ctx.auth.user.id),
+                    search ? ilike(meetings.name, `%${search}%`) : undefined,
+                    status ? eq(meetings.status, status) : undefined,
+                    agentId ? eq(meetings.agentId, agentId) : undefined
+                )
+            );
 
-      const totalPages = Math.ceil(total.count / pageSize);
+        const totalPages = Math.ceil(total.count / pageSize);
 
-      return {
-        items: data,
-        total: total.count,
-        totalPages,
-      };
+        return {
+            items: data,
+            total: total.count,
+            totalPages,
+        };
     }),
 });
--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,20 +1,28 @@
-'use client'
+"use client";
 
+import { DataTable } from "@/components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
-import { useTRPC } from "@/trpc/client"
+import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const MeetingsView = () => {
-    const trpc = useTRPC();
-    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
-    return (
-        <div>
-            {/* {JSON.stringify(data)} */}
-            meeting
-        </div>
-    );
-}
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+  return (
+    <div>
+      <DataTable data={data.items} columns={columns} />
+      {data.items.length === 0 && (
+        <EmptyState
+          title="Create your first meeting"
+          description="Schedule a meeting to start collaborating with your agents. Meetings can be used to discuss topics and share ideas."
+        />
+      )}
+    </div>
+  );
+};
 
 export const MeetingsViewLoading = () => {
   return (
